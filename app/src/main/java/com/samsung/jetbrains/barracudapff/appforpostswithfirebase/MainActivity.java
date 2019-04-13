77b88e4e04910cf639d10fd2aa384d53f7433a18
BarracudaPff <mrm.kikll@gmail.com>
2019-04-13 19:08:40 +0300

    update

--- app/src/main/java/com/samsung/jetbrains/barracudapff/appforpostswithfirebase/MainActivity.java
+++ app/src/main/java/com/samsung/jetbrains/barracudapff/appforpostswithfirebase/MainActivity.java
@@ -1,9 +1,11 @@
 package com.samsung.jetbrains.barracudapff.appforpostswithfirebase;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,7 +16,12 @@ import com.samsung.jetbrains.barracudapff.appforpostswithfirebase.models.Post;
 public class MainActivity extends AppCompatActivity {
 
     protected static final Query sChatQuery =
-            FirebaseDatabase.getInstance().getReference().child("chats").limitToLast(50);
+            FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child("posts")
+                    .limitToLast(50);
+
+    private PostsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +33,34 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseRecyclerOptions<Post> options = new FirebaseRecyclerOptions.Builder<Post>()
                 .setQuery(sChatQuery, Post.class)
-                .setLifecycleOwner(this)
                 .build();
 
-        PostsAdapter adapter = new PostsAdapter(options);
+        adapter = new PostsAdapter(options);
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton actionButton = findViewById(R.id.floatingActionButton);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = FirebaseDatabase.getInstance()
+                        .getReference()
+                        .child("posts")
+                        .push().getKey();
+
+                Post post = new Post(
+                        key,
+                        "name",
+                        "text",
+                        null,
+                        0,
+                        null
+                );
+
+                FirebaseDatabase.getInstance().getReference()
+                        .child("posts")
+                        .child(key)
+                        .setValue(post);
+            }
+        });
     }
 }
