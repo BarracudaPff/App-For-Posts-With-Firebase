f1a177c94b720da150576f55296b695a4110d69d
BarracudaPff <mrm.kikll@gmail.com>
2019-04-13 18:56:46 +0300

    update

--- app/src/main/java/com/samsung/jetbrains/barracudapff/appforpostswithfirebase/adapters/PostsAdapter.java
+++ app/src/main/java/com/samsung/jetbrains/barracudapff/appforpostswithfirebase/adapters/PostsAdapter.java
@@ -1,4 +1,31 @@
 package com.samsung.jetbrains.barracudapff.appforpostswithfirebase.adapters;
 
-public class PostsAdapter extends FirebaseAda {
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.samsung.jetbrains.barracudapff.appforpostswithfirebase.R;
+import com.samsung.jetbrains.barracudapff.appforpostswithfirebase.models.Post;
+import com.samsung.jetbrains.barracudapff.appforpostswithfirebase.viewHolders.PostViewHolder;
+
+public class PostsAdapter extends FirebaseRecyclerAdapter<Post, PostViewHolder> {
+    public PostsAdapter(@NonNull FirebaseRecyclerOptions<Post> options) {
+        super(options);
+    }
+
+    //Put data (model) in ViewHolder (layout)
+    @Override
+    protected void onBindViewHolder(@NonNull PostViewHolder holder, int position, @NonNull Post model) {
+        holder.onBind(model);
+    }
+
+    //Create ViewHolder
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return new PostViewHolder(LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_post, viewGroup, false));
+    }
 }
